07764609ffd11ebfdd16fe51d813707544548668
varun <varun.bhargava80@gmail.com>
2021-09-21 12:08:45 +1000

    Got it working

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,23 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return await Promise.all(urls.map(getQuoteSingle));
 };
+
+const getQuoteSingle = async (url) => {
+
+  try {
+    const result = await httpGet(url);
+    const messageBody = JSON.parse(result.body);
+        
+    return result.status === 200 
+      ? {"Arnie Quote": messageBody.message} 
+      : {"FAILURE": messageBody.message};
+  } catch (error) { 
+    console.log(error);
+    return { "FAILURE" : error.message };
+  }
+}
 
 module.exports = {
   getArnieQuotes,
